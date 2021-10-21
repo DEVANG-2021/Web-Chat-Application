f5e423897929f205fea5c915891d69763bd31d9d
Devang Parmar <85817421+DEVANG-2021@users.noreply.github.com>
2021-10-21 15:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Chat.html');
+    res.sendFile(__dirname + '/index.html');
   });
 
 const users={};
